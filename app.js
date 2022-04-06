d8976aa591de338f0102b2fbe07c6285faaa2689
HeoYeonJae <yeonvv8@gmail.com>
2022-04-06 19:11:25 +0900

    Update getContext

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 
 ctx.strokeStyle = "#2c2c2c";
-ctx.linewidth = 2.5;
+ctx.lineWidth = 2.5;
 
-canvas.width = 700;
-canvas.height = 700;
+canvas.width = document.getElementsByClassName("canvas")[0].offsetWidth;
+canvas.height = document.getElementsByClassName("canvas")[0].offsetHeight;
 
 let painting = false;
 
@@ -17,12 +17,12 @@ function startPainting() {
   painting = true;
 }
 
+// 마우스를 움직이는 내내 발생 //
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
   if (!painting) {
     ctx.beginPath();
-    ctx.moveTo(x, y);
   } else {
     ctx.lineTo(x, y);
     ctx.stroke();
